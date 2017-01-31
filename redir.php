215faf85da5b3995b875fba35fe80d3d1dcb76f4
Epidemics Scepticism <epidemics-scepticism@users.noreply.github.com>
2017-01-31 00:15:38 +0000

    Create redir.php

---
+++ redir.php
@@ -0,0 +1,33 @@
+<?php
+	$onion_url = "foobarbazqux.onion"; // Set appropriately
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Redir POC</title>
+<?php
+	if ($_SERVER['HTTP_HOST'] !== $onion_url) {
+?>
+<style>
+	.overlay {
+		visibility: hidden;
+	}
+</style>
+<link rel="stylesheet" type="text/css" href="<?php print "$onion_url"; ?>/redir.css">
+</head>
+<body>
+<script src="<?php print "$onion_url"; ?>/redir.js"></script>
+<div class="overlay">
+<p>You are using Tor please use the <a href="<?php print "$onion_url"; ?>">Onion Service.</a></p>
+</div>
+<?php
+	} else {
+?>
+</head>
+<body>
+<?php
+	}
+?>
+<p>Content Goes Here...</p>
+</body>
+</html>
